92a6dff9a6c7c00754dfa68d1ce092950fec0589
Temur <tim_1998@bk.ru>
2019-02-14 15:19:25 +0300

    Week #04 Assignment

---
+++ Week04/common.h
@@ -0,0 +1,16 @@
+typedef struct _test_struct{
+
+    char name[20];
+    unsigned int age;
+    char group[10];
+
+} test_struct_t;
+
+
+typedef struct result_struct_{
+
+  char name[20];
+  unsigned int age;
+  char group[10];
+
+} result_struct_t;
